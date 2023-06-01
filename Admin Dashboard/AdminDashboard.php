7378cf9033ccc533139d2ec8c00ec8043b24187d
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2023-06-01 19:44:27 +0300

    Added file on Admin Dashboard
    but it is still not done don't work on it I will finish it

---
+++ Admin Dashboard/AdminDashboard.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="AdminDashboard.css">
+</head>
+<body>
+    
+    <div class="wrapper">
+        <div class="header">
+        <nav class="navbar">
+        <div class="logo">
+          <img src="pics/trylogo.png" alt="">
+        </div>
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+          <ul>
+            <li><a href="http://localhost/Football-Club-Website/Home%20Page">HOME</a></li>
+            <li><a href="http://localhost/Football-Club-Website/merch/User/userstore.php">SHOP</a>
+             <!-- drop down -->
+             <!-- <ul>
+              <li><a href="">Merch</a></li>
+              <li><a href="">Tickets</a></li>
+             </ul> -->
+            </li>
+            <li><a href="http://localhost/Football-Club-Website/news/news.php">NEWS</a></li>
+            <li><a href="http://localhost/Football-Club-Website/fixtures/fixtures.php">FIXTURES</a></li>
+            <li><a href="http://localhost/Football-Club-Website/team%20members/user/userView.php">TEAM</a></li>
+            <li><a href="#">CLUB </a></li>
+            <li><a href="http://localhost/Football-Club-Website/merch/Login/login.html">Login</a></li>
+            <!-- drop down -->
+          </ul>
+        </div>
+      </nav>
+
+        <div class="dashboard">
+            <div class="item"><a href=""><img src="../fixtures/img/soccer-player.png" alt=""></a></div>
+            <div class="item"><a href=""><img src="../fixtures/img/soccer-game.png" alt=""></a></div>
+            <div class="item"><a href=""><img src="../fixtures/img/shopping-cart.png" alt=""></a></div>
+            <div class="item"><a href=""><img src="../fixtures/img/news.png" alt=""></a></div>
+           
+        </div>
+        <div class="main-section">
+            main section
+        </div>
+    </div>
+</body>
+</html>
